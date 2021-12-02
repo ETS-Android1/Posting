2c6b7b00fbc3bded2aba8dd103eefa5dd3143639
JUNU <57633459+wnsdn2186@users.noreply.github.com>
2021-12-03 02:06:47 +0900

    Update AWSKeys.java

--- app/src/main/java/com/example/post/aws/AWSKeys.java
+++ app/src/main/java/com/example/post/aws/AWSKeys.java
@@ -3,8 +3,8 @@ package com.example.post.aws;
 import com.amazonaws.regions.Regions;
 
 public class AWSKeys {
-    protected static final String COGNITO_POOL_ID = "ap-northeast-2:44d69883-49d2-4586-9c13-465f3e9cd278";
+    protected static final String COGNITO_POOL_ID = "";
     protected static final Regions MY_REGION = Regions.AP_NORTHEAST_2; /*Change Region Here*/
-    public static final String BUCKET_NAME = "sikigobucket";
+    public static final String BUCKET_NAME = "";
 
 }
